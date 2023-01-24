0530fcfeb22846cf1e42e6fb5b346daceb741ba9
Beatrice Abrahamsson <beaabrahamsson@hotmail.com>
2023-01-24 12:48:48 +0100

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function mobileNav() {
+    var nav = document.getElementById("myLinks");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
